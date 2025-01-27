137c6f755196d2362e7b686f0d8dcb8855c49feb
Minhajul Islam <mmminhaj221@gmail.com>
2025-01-27 16:06:00 +0600

    Create problem1.cpp

---
+++ level_800/problem1.cpp
@@ -0,0 +1,27 @@
+// problem link : https://codeforces.com/problemset/problem/617/A
+
+#include <iostream>
+using namespace std ;
+
+void calc (int &remaining, int &steps, int n ) {
+	remaining -= n ;
+	steps++ ;
+}
+int main()
+{
+	int x , steps = 0 ;
+	cin >> x ;
+	int remaining = x;
+
+	while (remaining > 1) {
+		if(remaining == 1) calc(remaining,steps,1);
+		if(remaining == 2) calc(remaining,steps,2);
+		if(remaining == 3) calc(remaining,steps,3);
+		if(remaining == 4) calc(remaining,steps,4);
+		if(remaining >= 5) calc(remaining,steps,5);
+	}
+
+	cout << steps ;
+
+	return 0;
+}
