0710a21d0f4608d1fc4784fce7f6ef4247e730bb
Minhajul Islam <mmminhaj221@gmail.com>
2025-01-29 10:44:09 +0600

    Update problem3.cpp

--- level_800/problem3.cpp
+++ level_800/problem3.cpp
@@ -1,1 +1,18 @@
+// Problem link : https://codeforces.com/problemset/problem/236/A
 
+#include <iostream>
+using namespace std ;
+int main () {
+    
+    string word , dis = "" ;
+    cin >> word ;
+    
+    for(int i = 0 ; i<word.length() ; i++) {
+        if( dis.find(word[i]) == string::npos ) dis += word[i] ;
+    }
+    
+    if (dis.length() % 2 == 0) cout << "CHAT WITH HER!" ;
+    else cout << "IGNORE HIM!" ;
+    
+    return 0 ;
+}
